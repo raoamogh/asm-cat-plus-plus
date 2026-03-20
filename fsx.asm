9d26b367ce432a060112bec446ac29d5c63cd289
raoamogh <amoghagrao@gmail.com>
2026-03-20 19:52:11 +0530

    added bench and line numbering features

--- fsx.asm
+++ fsx.asm
@@ -1,303 +1,344 @@
 section .data
-    msg_lines db "Lines: "
-    len_lines equ $ - msg_lines
-    msg_words db "Words: "
-    len_words equ $ - msg_words
-    msg_bytes db "Bytes: "
-    len_bytes equ $ - msg_bytes
-    newline db 10
-    err_msg db "Error: cannot open file", 10
-    err_len equ $ - err_msg
-    stats_flag db "--stats", 0
-    highlight_prefix db "--highlight=", 0
-    red_start db 27,"[31m"
-    red_len equ $ - red_start
-    red_end db 27,"[0m"
-    red_end_len equ $ - red_end
+    msg_bench_cyc db "Bench - CPU Cycles: ", 0
+    msg_bench_time db "Bench - Approx Time: ", 0
+    msg_ms        db " ms", 10, 0
+    newline       db 10
+    highlight_pfx db "--highlight=", 0
+    ic_flag       db "--ignore-case", 0
+    ln_flag       db "-n", 0
+    bn_flag       db "--bench", 0
+    red_s         db 27,"[31m"
+    red_s_len     equ $ - red_s
+    red_e         db 27,"[0m"
+    red_e_len     equ $ - red_e
+    err_msg       db "Error: Operation failed", 10
+    err_len       equ $ - err_msg
+    colon_sep     db ": ", 0
 
 section .bss
-    buffer resb 4096
-    num_buf resb 32
-    current_idx resq 1
+    num_buf          resb 32
+    current_idx      resq 1
+    ignore_case_bool resb 1
+    line_num_bool    resb 1
+    bench_bool       resb 1
+    start_cycles     resq 1
+    file_size        resq 1
+    mmap_ptr         resq 1
+    pattern_ptr      resq 1
+    line_counter     resq 1
 
 section .text
     global _start
 
 _start:
-    mov rbx, [rsp]
-    cmp rbx, 2
+    mov rbp, [rsp]
+    cmp rbp, 2
     jl exit_program
 
-    mov rdi, [rsp+16]
-    mov rsi, stats_flag
+    mov qword [current_idx], 1
+    mov byte [ignore_case_bool], 0
+    mov byte [line_num_bool], 0
+    mov byte [bench_bool], 0
+    mov qword [pattern_ptr], 0
+
+.parse_args:
+    mov rcx, [current_idx]
+    cmp rcx, rbp
+    jge .execute_prep
+    
+    mov rdi, [rsp + rcx*8 + 8]
+    
+    mov rsi, ic_flag
     call strcmp
     test rax, rax
-    je stats_init
+    jz .set_ic
 
-    mov rdi, [rsp+16]
-    mov rsi, highlight_prefix
+    mov rcx, [current_idx]
+    mov rdi, [rsp + rcx*8 + 8]
+    mov rsi, ln_flag
+    call strcmp
+    test rax, rax
+    jz .set_ln
+
+    mov rcx, [current_idx]
+    mov rdi, [rsp + rcx*8 + 8]
+    mov rsi, bn_flag
+    call strcmp
+    test rax, rax
+    jz .set_bn
+
+    mov rcx, [current_idx]
+    mov rdi, [rsp + rcx*8 + 8]
+    mov rsi, highlight_pfx
     mov rdx, 12
     call strncmp
     test rax, rax
-    je highlight_init
+    jz .set_hl
 
-    mov qword [current_idx], 2
-    jmp default_loop
+    mov rcx, [current_idx]
+    mov rdi, [rsp + rcx*8 + 8]
+    cmp byte [rdi], '-'
+    jne .execute_prep
 
-highlight_init:
-    mov r14, [rsp+16]
-    add r14, 12
-    mov qword [current_idx], 3
-    jmp highlight_loop
-
-stats_init:
-    mov qword [current_idx], 3
-    jmp stats_loop
-
-default_loop:
-    mov rbx, [rsp]
-    mov rsi, [current_idx]
-    cmp rsi, rbx
-    jg exit_program
-    mov rdi, [rsp + rsi*8]
-    mov rax, 2
-    xor rsi, rsi
-    syscall
-    cmp rax, 0
-    jl open_error_d
-    mov r15, rax
-.r:
-    mov rax, 0
-    mov rdi, r15
-    mov rsi, buffer
-    mov rdx, 4096
-    syscall
-    cmp rax, 0
-    jle .c
-    mov rdx, rax
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, buffer
-    syscall
-    jmp .r
-.c:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, newline
-    mov rdx, 1
-    syscall
-    mov rax, 3
-    mov rdi, r15
-    syscall
     inc qword [current_idx]
-    jmp default_loop
+    jmp .parse_args
 
-highlight_loop:
-    mov rbx, [rsp]
-    mov rsi, [current_idx]
-    cmp rsi, rbx
-    jg exit_program
-    mov rdi, [rsp + rsi*8]
-    mov rax, 2
-    xor rsi, rsi
-    syscall
-    cmp rax, 0
-    jl open_error_h
-    mov r15, rax
-.read:
-    mov rax, 0
-    mov rdi, r15
-    mov rsi, buffer
-    mov rdx, 4096
-    syscall
-    cmp rax, 0
-    jle .close
-    mov r13, rax
-    xor r12, r12
-.scan:
-    cmp r12, r13
-    jge .read
-    mov rbx, r14
-    mov r10, r12
-    xor rbp, rbp
-.match:
-    movzx rdx, byte [rbx]
-    test rdx, rdx
-    jz .found
-    cmp r10, r13
-    je .no_match
-    movzx rax, byte [buffer + r10]
-    mov r9, rdx
-    cmp r9, 'A'
-    jl .c1
-    cmp r9, 'Z'
-    jg .c1
-    or r9, 0x20
-.c1:
-    mov r11, rax
-    cmp r11, 'A'
-    jl .c2
-    cmp r11, 'Z'
-    jg .c2
-    or r11, 0x20
-.c2:
-    cmp r9, r11
-    jne .no_match
-    inc rbx
-    inc r10
-    inc rbp
-    jmp .match
-.found:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, red_start
-    mov rdx, red_len
-    syscall
-    mov rax, 1
-    mov rdi, 1
-    lea rsi, [buffer + r12]
-    mov rdx, rbp
-    syscall
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, red_end
-    mov rdx, red_end_len
-    syscall
-    add r12, rbp
-    jmp .scan
-.no_match:
-    mov rax, 1
-    mov rdi, 1
-    lea rsi, [buffer + r12]
-    mov rdx, 1
-    syscall
-    inc r12
-    jmp .scan
-.close:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, newline
-    mov rdx, 1
-    syscall
-    mov rax, 3
-    mov rdi, r15
-    syscall
+.set_ic:
+    mov byte [ignore_case_bool], 1
     inc qword [current_idx]
-    jmp highlight_loop
+    jmp .parse_args
+.set_ln:
+    mov byte [line_num_bool], 1
+    inc qword [current_idx]
+    jmp .parse_args
+.set_bn:
+    mov byte [bench_bool], 1
+    inc qword [current_idx]
+    jmp .parse_args
+.set_hl:
+    mov rcx, [current_idx]
+    mov rdi, [rsp + rcx*8 + 8]
+    add rdi, 12
+    mov [pattern_ptr], rdi
+    inc qword [current_idx]
+    jmp .parse_args
 
-stats_loop:
-    mov rbx, [rsp]
-    mov rsi, [current_idx]
-    cmp rsi, rbx
-    jg exit_program
-    mov rdi, [rsp + rsi*8]
-    mov rax, 2
-    xor rsi, rsi
-    syscall
-    cmp rax, 0
-    jl open_error_s
-    mov r15, rax
-    xor r8, r8
-    xor r9, r9
-    xor r10, r10
-    xor r11, r11
-.read:
-    mov rax, 0
-    mov rdi, r15
-    mov rsi, buffer
-    mov rdx, 4096
-    syscall
-    cmp rax, 0
-    jle .done
-    add r8, rax
-    xor rcx, rcx
-.l:
-    cmp rcx, rax
-    jge .read
-    movzx rbx, byte [buffer + rcx]
-    cmp rbx, 10
-    jne .w
-    inc r9
-.w:
-    cmp rbx, 32
-    je .is_w
-    cmp rbx, 10
-    je .is_w
-    cmp rbx, 9
-    je .is_w
-    test r11, r11
-    jnz .next
-    inc r10
-    mov r11, 1
-    jmp .next
-.is_w:
-    xor r11, r11
-.next:
-    inc rcx
-    jmp .l
-.done:
-    mov rax, 3
-    mov rdi, r15
-    syscall
+.execute_prep:
+    cmp byte [bench_bool], 1
+    jne .process_files
+    rdtsc
+    shl rdx, 32
+    or rax, rdx
+    mov [start_cycles], rax
+
+.process_files:
+    mov rcx, [current_idx]
+    mov rbp, [rsp]
+    cmp rcx, rbp
+    jge .finish
+
+    mov rdi, [rsp + rcx*8 + 8]
+    call process_single_file
+    
+    inc qword [current_idx]
+    jmp .process_files
+
+.finish:
+    cmp byte [bench_bool], 1
+    jne exit_program
+    rdtsc
+    shl rdx, 32
+    or rax, rdx
+    sub rax, [start_cycles]
+    mov r15, rax ; Save total cycles
+
+    ; Print Cycles
     mov rax, 1
     mov rdi, 1
-    mov rsi, msg_lines
-    mov rdx, len_lines
+    mov rsi, msg_bench_cyc
+    mov rdx, 20
     syscall
-    mov rax, r9
+    mov rax, r15
     call print_number
+
+    ; Print Approx Time (Cycles / 3,000,000 approx for ms on 3GHz)
     mov rax, 1
     mov rdi, 1
-    mov rsi, msg_words
-    mov rdx, len_words
+    mov rsi, msg_bench_time
+    mov rdx, 21
     syscall
-    mov rax, r10
-    call print_number
+    mov rax, r15
+    xor rdx, rdx
+    mov rbx, 3000000
+    div rbx
+    call print_num_raw
     mov rax, 1
     mov rdi, 1
-    mov rsi, msg_bytes
-    mov rdx, len_bytes
+    mov rsi, msg_ms
+    mov rdx, 4
     syscall
-    mov rax, r8
-    call print_number
-    inc qword [current_idx]
-    jmp stats_loop
-
-open_error_d:
-    mov rax, 1
-    mov rdi, 2
-    mov rsi, err_msg
-    mov rdx, err_len
-    syscall
-    inc qword [current_idx]
-    jmp default_loop
-
-open_error_h:
-    mov rax, 1
-    mov rdi, 2
-    mov rsi, err_msg
-    mov rdx, err_len
-    syscall
-    inc qword [current_idx]
-    jmp highlight_loop
-
-open_error_s:
-    mov rax, 1
-    mov rdi, 2
-    mov rsi, err_msg
-    mov rdx, err_len
-    syscall
-    inc qword [current_idx]
-    jmp stats_loop
 
 exit_program:
     mov rax, 60
     xor rdi, rdi
     syscall
 
+process_single_file:
+    mov rax, 2
+    xor rsi, rsi
+    syscall
+    cmp rax, 0
+    jl .err
+    mov r15, rax
+
+    mov rax, 8
+    mov rdi, r15
+    xor rsi, rsi
+    mov rdx, 2
+    syscall
+    mov [file_size], rax
+    test rax, rax
+    jz .close_f
+
+    mov rax, 8
+    mov rdi, r15
+    xor rsi, rsi
+    xor rdx, rdx
+    syscall
+
+    mov rax, 9
+    xor rdi, rdi
+    mov rsi, [file_size]
+    mov rdx, 1
+    mov r10, 2
+    mov r8, r15
+    xor r9, r9
+    syscall
+    cmp rax, -4095
+    jae .err
+    mov [mmap_ptr], rax
+
+    mov r12, rax
+    mov r13, rax
+    add r13, [file_size]
+    mov qword [line_counter], 1
+    mov r14, 1
+
+.scan:
+    cmp r12, r13
+    jge .done_file
+
+    cmp byte [line_num_bool], 1
+    jne .check_hl
+    test r14, r14
+    jz .check_hl
+    mov rax, [line_counter]
+    call print_num_raw
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, colon_sep
+    mov rdx, 2
+    syscall
+    xor r14, r14
+
+.check_hl:
+    mov rdi, [pattern_ptr]
+    test rdi, rdi
+    jz .plain
+    call regex_match
+    test rax, rax
+    jz .plain
+
+    mov rbx, rax
+    push rax
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, red_s
+    mov rdx, red_s_len
+    syscall
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, r12
+    mov rdx, rbx
+    syscall
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, red_e
+    mov rdx, red_e_len
+    syscall
+    pop rax
+    add r12, rax
+    jmp .scan
+
+.plain:
+    movzx rax, byte [r12]
+    cmp al, 10
+    jne .out
+    inc qword [line_counter]
+    mov r14, 1
+.out:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, r12
+    mov rdx, 1
+    syscall
+    inc r12
+    jmp .scan
+
+.done_file:
+    ; Fix the percentage symbol by ensuring newline at end of every file output
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, newline
+    mov rdx, 1
+    syscall
+
+    mov rax, 11
+    mov rdi, [mmap_ptr]
+    mov rsi, [file_size]
+    syscall
+.close_f:
+    mov rax, 3
+    mov rdi, r15
+    syscall
+    ret
+.err:
+    mov rax, 1
+    mov rdi, 2
+    mov rsi, err_msg
+    mov rdx, err_len
+    syscall
+    ret
+
+regex_match:
+    xor rcx, rcx
+.lp:
+    movzx r8, byte [rdi + rcx]
+    test r8, r8
+    jz .ok
+    lea rdx, [r12 + rcx]
+    cmp rdx, r13
+    jge .fail
+    movzx r9, byte [rdx]
+    
+    cmp r8, '.'
+    je .nxt
+
+    push r8
+    push r9
+    cmp byte [ignore_case_bool], 1
+    jne .st
+    
+    ; Normalize r8
+    cmp r8, 'A'
+    jl .c1
+    cmp r8, 'Z'
+    jg .c1
+    or r8, 0x20
+.c1:
+    ; Normalize r9
+    cmp r9, 'A'
+    jl .c2
+    cmp r9, 'Z'
+    jg .c2
+    or r9, 0x20
+.c2:
+.st:
+    cmp r8, r9
+    pop r9
+    pop r8
+    jne .fail
+.nxt:
+    inc rcx
+    jmp .lp
+.ok: mov rax, rcx
+    ret
+.fail: xor rax, rax
+    ret
+
 strcmp:
     xor rax, rax
-.loop:
+.l:
     mov al, [rdi]
     mov bl, [rsi]
     cmp al, bl
@@ -306,17 +347,15 @@ strcmp:
     jz .eq
     inc rdi
     inc rsi
-    jmp .loop
-.ne:
-    mov rax, 1
+    jmp .l
+.ne: mov rax, 1
     ret
-.eq:
-    xor rax, rax
+.eq: xor rax, rax
     ret
 
 strncmp:
     xor rax, rax
-.loop:
+.l:
     test rdx, rdx
     jz .eq
     mov al, [rdi]
@@ -328,38 +367,40 @@ strncmp:
     inc rdi
     inc rsi
     dec rdx
-    jmp .loop
-.ne:
-    mov rax, 1
+    jmp .l
+.ne: mov rax, 1
     ret
-.eq:
-    xor rax, rax
+.eq: xor rax, rax
     ret
 
-print_number:
+print_num_raw:
     mov rbx, 10
     lea rsi, [num_buf + 31]
     mov byte [rsi], 0
-    mov r12, rsi
+    mov r8, rsi
     test rax, rax
-    jnz .conv
+    jnz .c
     dec rsi
     mov byte [rsi], '0'
-    jmp .out
-.conv:
+    jmp .o
+.c:
     xor rdx, rdx
     div rbx
     add dl, '0'
     dec rsi
     mov [rsi], dl
     test rax, rax
-    jnz .conv
-.out:
-    mov rdx, r12
+    jnz .c
+.o:
+    mov rdx, r8
     sub rdx, rsi
     mov rax, 1
     mov rdi, 1
     syscall
+    ret
+
+print_number:
+    call print_num_raw
     mov rax, 1
     mov rdi, 1
     mov rsi, newline
